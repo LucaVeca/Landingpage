f3b67ffa18683e5ec848b28105afed1221c0f5ad
Luca Veca <lucaveca@gmail.com>
2015-05-21 12:20:21 -0700

    Style

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+
+<html>
+<div class="page">
+	<head>
+		<link type="text/css" rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" type="text/css"  href="css/bootstrap.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Pragati+Narrow' rel='stylesheet' type='text/css'>
+		<title></title>
+	</head>
+	<body>
+		<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,400italic' rel='stylesheet' type='text/css'>
+
+<nav class="Quick-navigation">
+  <a href="#A" class="Quick-navigation-item">1</a>
+  <a href="#B" class="Quick-navigation-item">2</a>
+  <a href="#C" class="Quick-navigation-item">3</a>
+  <a href="#D" class="Quick-navigation-item">4</a>
+  <a href="#E" class="Quick-navigation-item">5</a>
+  <div class="Scroll-progress-indicator"></div>
+</nav>
+
+<section id="A" class="js-scroll-step">
+  <div class="icon1">
+		<div class="text2">PASADENA, CA
+			</div>	
+			<div width="150px">
+			<img class="space" src="images/social122.png" alt="" style="width:24px;height:24px">
+			<img class="space" src="images/google124.png" alt="" style="width:24px;height:24px">
+			<img class="space" src="images/social135.png" alt="" style="width:24px;height:24px">
+			<img class="space" src="images/social136.png" alt="" style="width:24px;height:24px">
+			<img class="space" src="images/instagram19.png" alt="" style="width:24px;height:24px">
+			</div>
+		</div>
+		<center class="name">LUCA VECA<center class="type">WEBSITE DESIGNER | FRONT END DEVELOPER</center></center>
+		
+		<hr class="line"w idth="80%">
+</section>
+
+<section id="B"  class="js-scroll-step">
+  <div class="icon1">
+		
+		<center class="name">LUCA VECA<center class="type">WEBSITE DESIGNER | FRONT END DEVELOPER</center></center>
+		
+		<hr class="line"w idth="80%">
+</section>
+
+<section id="C" class="js-scroll-step">
+  <div class="icon1">
+		
+		<center class="name">LUCA VECA<center class="type">WEBSITE DESIGNER | FRONT END DEVELOPER</center></center>
+		
+		<hr class="line"w idth="80%">
+</section>
+
+<section id="D" class="js-scroll-step">
+  <div class="icon1">
+		
+		<center class="name">LUCA VECA<center class="type">WEBSITE DESIGNER | FRONT END DEVELOPER</center></center>
+		
+		<hr class="line"w idth="80%">
+</section>
+
+<section id="E" class="js-scroll-step">
+  
+		<center class="name">LUCA VECA<center class="type">WEBSITE DESIGNER | FRONT END DEVELOPER</center></center>
+		
+		<hr class="line"w idth="80%">
+</section>
+
+<button class="Scroll-to-top">Scroll To Top</button>
+		
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	</body>
+	<!-- <div>Icons made by <a href="http://www.flaticon.com/authors/alessio-atzeni" title="Alessio Atzeni">Alessio Atzeni</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div> 
+	<div>Icons made by <a href="http://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
+	<div>Icons made by <a href="http://www.flaticon.com/authors/alessio-atzeni" title="Alessio Atzeni">Alessio Atzeni</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
+	<div>Icons made by <a href="http://www.flaticon.com/authors/alessio-atzeni" title="Alessio Atzeni">Alessio Atzeni</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
+	<div>Icons made by <a href="http://www.flaticon.com/authors/bogdan-rosu" title="Bogdan Rosu">Bogdan Rosu</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>-->
+	</div>
+</html>
